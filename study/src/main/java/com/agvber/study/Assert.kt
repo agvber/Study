1f692697f214b621546ec34ee6d53f0ddad0f7c8
agvber <agvber12@gmail.com>
2025-10-06 18:24:44 +0900

    에러 메시지 추가

--- study/src/main/java/com/agvber/study/Assert.kt
+++ study/src/main/java/com/agvber/study/Assert.kt
@@ -3,6 +3,8 @@ package com.agvber.study
 object Assert {
 
     fun <T> equal(actual: T, expect: T) {
-        require(actual == expect)
+        require(actual == expect) {
+            "UnExpect actual: $actual expect: $expect"
+        }
     }
 }
